b04f79aac4dbb6c5b7ae31c94045cb36cb3c509f
lukamicahzamani <149020825+luka-micah@users.noreply.github.com>
2026-07-09 16:34:16 -0400

    Update: added scroll up feature on every page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,11 @@ import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import AppRoutes from './routes/AppRoutes';
 import GTMProvider from './components/analytics/GTMProvider';
+import ScrollToTop from './components/common/ScrollToTop';
 
 const App = () => (
   <BrowserRouter>
+    <ScrollToTop />
     <GTMProvider>
       <AuthProvider>
         <AppRoutes />
